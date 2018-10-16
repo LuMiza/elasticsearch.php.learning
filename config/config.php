171371a01b32135dd109b6c2c23e6ae3d39526ee
LuMiza <26994070434@qq.com>
2018-10-16 16:52:41 +0800

    对elasticsearch模块化

--- config/config.php
+++ config/config.php
@@ -6,11 +6,10 @@ return [
         '192.168.80.139:9200'
     ],
     'medoo' => [
-        //必需mysql.exe -h120.79.212.4 -P3306 -uroot -p"O@o&WL1#zkpSXJGiqIHK"
         'database_type'=>'mysql',
         'database_name'=>'*',      //数据库名称
         'server'=>'*',        //数据库连接地址
-        'username'=>'root',  //用户名
+        'username'=>'&',  //用户名
         'password'=>'*',  //密码
         'charset'=>'utf8',            //数据库编码
         // [可选的] 数据库连接端口
